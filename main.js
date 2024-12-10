01211df2393f68c2373d59bfc67298104d141987
Sita Cicirko <ns.cicirko@gmail.com>
2024-12-10 13:26:02 +0000

    last changes

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-topButton.onclick = function () {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
+import { createRoot } from "react-dom/client";
+
+document.body.innerHTML = '<div id="app"></div>';
+
+const root = createRoot(document.getElementById("app"));
+root.render(<h1>Hello, world</h1>);
